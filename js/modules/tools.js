107a8331628fc8d98dc51860ba36179197b43063
fishfriday <info@fishfriday.nl>
2018-04-19 15:40:46 +0200

    Added new functions for fetching the values of a form

--- js/modules/tools.js
+++ js/modules/tools.js
@@ -30,8 +30,9 @@
  * Find the element with a CSS class
  *
  * @function
- * @param   {HTMLElement} start - Element to start from
- * @param   {String} cls - CSS class to find the element with
+ * @since   1.0
+ * @param   {HTMLElement} start Element to start from
+ * @param   {String} cls CSS class to find the element with
  * @returns {HTMLElement}
  */	
 const getParent = (start, cls) => {
@@ -40,15 +41,16 @@ const getParent = (start, cls) => {
         el = el.parentElement;
     }
     return el;
-}
+};
 
 /**
  * Get element by walking over the next siblings
  * Find the element with a CSS class
  *
  * @function
- * @param   {HTMLElement} start - Element to start from
- * @param   {String} cls - CSS class to find the element with
+ * @since   1.0
+ * @param   {HTMLElement} start Element to start from
+ * @param   {String} cls CSS class to find the element with
  * @returns {HTMLElement}
  */	
 const getNextSibling = (start, cls) => {
@@ -57,15 +59,16 @@ const getNextSibling = (start, cls) => {
         el = el.nextElementSibling;
     }
     return el;
-}
+};
 
 /**
  * Get element by walking over the previous siblings
  * Find the element with a CSS class
  *
  * @function
- * @param   {HTMLElement} start - Element to start from
- * @param   {String} cls - CSS class to find the element with
+ * @since   1.0
+ * @param   {HTMLElement} start Element to start from
+ * @param   {String} cls CSS class to find the element with
  * @returns {HTMLElement}
  */	
 const getPrevSibling = (start, cls) => {
@@ -74,7 +77,7 @@ const getPrevSibling = (start, cls) => {
         el = el.previousElementSibling;
     }
     return el;
-}
+};
 
 /**
  * Get all the siblings of given element
@@ -82,8 +85,9 @@ const getPrevSibling = (start, cls) => {
  * Then it filters out the start element
  * 
  * @function
- * @param   {HTMLElement} start  - Element to start from
- * @returns {Array} - Array of found siblings
+ * @since   1.0
+ * @param   {HTMLElement} start  Element to start from
+ * @returns {Array} Array of found siblings
  */
 const getSiblings = (start) => {
     let el = start,
@@ -95,21 +99,100 @@ const getSiblings = (start) => {
         });
     }
     return sibs;
-}
+};
+
+/**
+ * Returns an object with all the values of a form
+ * 
+ * @function
+ * @since   1.0
+ * @param   {HTMLFormElement} form Form element with elements
+ * @returns {Object} Object with name/value pairs
+ */
+const getValues = (form) => {
+    let values = {};
+    for (let i = 0; i < form.elements.length; i += 1) {
+        if (form.elements[i].type !== 'submit') {
+            values[form.elements[i].name] = form.elements[i].value;
+        }
+    }
+    return values;
+};
 
 /**
  * Return an object with the selected fields
  *
  * @function
- * @param   {HTMLFormElement} form - Form element with elements
- * @returns {Object} - Object with name/value pairs
+ * @since   1.0
+ * @param   {HTMLFormElement} form Form element with elements
+ * @returns {Object} Object with name/value pairs
  */
 const getSelectedValues = (form) => {
     let values = {};
     for (let i = 0; i < form.elements.length; i += 1) {
-        if (form.elements[i].tagName === 'SELECT' && form.elements[i].value !== '') {
-            values[form.elements[i].name] = form.elements[i].value;
+        let el = form.elements[i];
+        if (
+            el.tagName === 'SELECT' && 
+            el.value !== ''
+        ) {
+            values[el.name] = el.value;
         }
     }
     return values;
+};
+
+/**
+ * Return an object with the checked inputs
+ *
+ * @function
+ * @since   1.0
+ * @param   {HTMLFormElement} form Form element with elements
+ * @returns {Object} Object with name/value pairs
+ */
+const getCheckedInputs = (form) => {
+    let values = {};
+    for (let i = 0; i < form.elements.length; i += 1) {
+        let el = form.elements[i];
+        if (
+            el.tagName === 'INPUT' && 
+            (el.type === 'checkbox' || el.type === 'radio') &&
+            el.checked === true
+        ) {
+            values[el.name] = el.value;
+        }
+    }
+    return values;
+};
+
+/**
+ * Returns an array with objects with values
+ * 
+ * @function
+ * @since   1.0
+ * @param {HTMLFormElement} form Form element
+ * @returns {Array} Array with objects
+ */
+const getValuesPerFieldset = (form) => {
+    let fieldsets = [];
+    for (let i = 0; i < form.elements.length; i += 1) {
+        if (form.elements[i].type === 'fieldset') {
+            let values = {},
+                fs = form.elements[i];
+            for (let j = 0; j < fs.elements.length; j += 1) {
+                let el = fs.elements[j];
+                if (
+                    (
+                        el.tagName === 'INPUT' ||
+                        el.tagName === 'SELECT' ||
+                        el.tagName === 'TEXTAREA'
+                    ) && 
+                    el.type !== 'submit'
+                ) {
+                    values[el.name] = el.value;
+                }
+            }
+            fieldsets.push(values);
+        }
+    }
+    return fieldsets;
 };
