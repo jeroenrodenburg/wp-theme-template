a869fd63c2572130d7915fba1bda7362d3933dff
controldigital <info@controldigital.nl>
2018-04-22 10:43:49 +0200

    Added new functions for adding and removing stuff

--- js/modules/google-maps.js
+++ js/modules/google-maps.js
@@ -1,3 +1,5 @@
+/* jshint esversion: 6 */
+
 /**
  *	@author Control <info@controldigital.nl>
  *	@file google-maps.js
@@ -24,6 +26,59 @@
  *	SOFTWARE.
  */
 
+/**
+ * createMap
+ * 
+ * Create a new map with the Google Maps JS API.
+ * Takes a string or HTMLElement to select the element with the map.
+ * 
+ * @function
+ * @since 	1.0
+ * @param 	{(String|HTMLElement)} mapElement The map element to select
+ * @param		{Object=} options Options for the google.maps.Map class
+ * @returns {<google.maps.Map>}
+ */
+const createMap = (mapElement, options = {center: {lat: 52.3499843, lng: 4.9163333}, zoom: 16}) => {
+
+	// Get the map elements
+	let element;
+	if (mapElement) {
+		if ('string' === typeof mapElement) {
+			element = document.querySelector(mapElement);
+		} else if (mapElement instanceof HTMLElement) {
+			element = mapElement;
+		}
+	} else {
+		return;
+	}
+
+	// Create new map
+	let map = new google.maps.Map(element, options);
+
+	// Set array for markers
+	map.markers = [];
+
+	// Set array for polylines
+	map.polylines = [];
+
+	// Set array for polygons
+	map.polygons = [];
+
+	// Set bounds for map
+	map.bounds = new google.maps.LatLngBounds();
+	
+	// Set infowindow for map
+	map.infowindow = null;
+
+	// Close infowindows on map click
+	google.maps.event.addListener(map, 'click', () => {
+		if (map.infowindow) map.infowindow.close();
+	});
+
+	// Return the map
+	return map;
+
+};
 
 /**
  * attachInfoWindow
@@ -45,7 +100,7 @@ const attachInfoWindow = (marker, content, map) => {
 		map.infowindow.open(map, marker);
 	});
 	
-}
+};
 
 /**
  * addMarkers
@@ -95,16 +150,16 @@ const addMarkers = (markers, map) => {
  * 
  * @function
  * @since 	1.0
- * @param 	{Object} marker 
+ * @param 	{Object} position 
  * @param 	{<google.maps.Map>} map 
  * @returns {Promise} returns the map object on resolve
  */
-const addMarker = (marker, map) => {
+const addMarker = (position, map) => {
 	return new Promise((resolve, reject) => {
-		if (marker && map) {
+		if (position && map) {
 
 			// Create coordinates
-			const latLng = new google.maps.LatLng(marker.lat, marker.lng);
+			const latLng = new google.maps.LatLng(position.lat, position.lng);
 
 			// Create marker
 			const marker = new google.maps.Marker({
@@ -150,6 +205,9 @@ const removeMarkers = (map) => {
 			// Remove markers from array
 			map.markers = [];
 
+			// Resolve with map
+			resolve(map);
+
 		} else {
 
 			// Reject with an error
@@ -160,48 +218,173 @@ const removeMarkers = (map) => {
 };
 
 /**
- * Create a new map
+ * addPolyline
+ * 
+ * Creates a polyline shape on the map.
+ * A polyline is a simple line drawn with an array of coordinates.
+ * 
+ * @example
+ * let coords = [
+ * 	{lat: 37.772, lng: -122.214},
+ * 	{lat: 21.291, lng: -157.821},
+ * 	{lat: -18.142, lng: 178.431}
+ * ];
+ * 
+ * addPolyline(coords, map);
+ * 
+ * @function
+ * @since		1.0
+ * @param 	{Object[]} coordinates Array of objects with lat and lng coordinates
+ * @param 	{<google.maps.Map>} map Google maps map instance
+ * @param 	{Object=} options 
+ * @returns	{Promise}
+ */
+const addPolyline = (coordinates, map, options = {geodesic: true, strokeColor: '#ff0000', strokeOpacity: 1.0, strokeWeight: 2}) => {
+	return Promise((resolve, reject) => {
+		if (coordinates && map) {
+
+			// Add coordinates to options
+			options.path = coordinates;
+
+			// Create new Polyline
+			const polyline = new google.maps.Polyline(options);
+
+			// Push to polylines
+			map.polylines.push(polyline);
+
+			// Set the polyline to the map
+			polyline.setMap(map);
+
+			// Return the map
+			resolve(map);
+
+		} else {
+
+			// Reject with an error
+			reject(new Error('coordinates and/or map arguments not given'));
+
+		}
+	});
+};
+
+/**
+ * Remove polylines from map
  * 
  * @function
  * @since 	1.0
- * @param 	{String} mapElement - The map element to select
- * @returns {<google.maps.Map>}
+ * @param 	{<google.maps.Map>} map 
+ * @returns {Promise} returns the map object on resolve
  */
-const createMaps = (mapElement) => {
+const removePolylines = (map) => {
+	return new Promise((resolve, reject) => {
+		if (map) {
 
-	// Get the map elements
-	const element = document.querySelector(mapElement);
+			// Loop over the polylines
+			map.polylines.forEach((polyline) => {
+				polyline.setMap(null);
+			});
 
-	// Arguments for map
-	let args = {
-		center: {
-			lat: 52.3499843,
-			lng: 4.9163333
-		},
-		zoom: 16
-	};
+			// Remove markers from array
+			map.polylines = [];
 
-	// Create new map
-	let map = new google.maps.Map(element, args);
+			// Resolve with map
+			resolve(map);
 
-	// Set array for markers
-	map.markers = [];
+		} else {
 
-	// Set bounds for map
-	map.bounds = new google.maps.LatLngBounds();
-	
-	// Set infowindow for map
-	map.infowindow = null;
+			// Reject with an error
+			reject(new Error('map argument not given'));
 
-	// Close infowindows on map click
-	google.maps.event.addListener(map, 'click', () => {
-		if (map.infowindow) map.infowindow.close();
+		}
 	});
-
-	// Return the map
-	return map;
-
 };
+
+/**
+ * addPolygon
+ * 
+ * Creates a polygon shape on the map.
+ * In contrast to the polyline a polygon always closes its shape.
+ * The colors and properties of the line can be adjusted with the options argument.
+ * 
+ * @example
+ * let coords = [
+ * 	{lat: 37.772, lng: -122.214},
+ * 	{lat: 21.291, lng: -157.821},
+ * 	{lat: -18.142, lng: 178.431},
+ * 	{lat: 37.772, lng: -122.214}
+ * ];
+ * 
+ * addPolygone(coords, map);
+ * 
+ * @function
+ * @since		1.0
+ * @param 	{Object[]} coordinates Array of objects with lat and lng coordinates
+ * @param 	{<google.maps.Map>} map Google maps map instance
+ * @param 	{Object=} options 
+ * @returns	{Promise}
+ */
+const addPolygon = (coordinates, map, options = {strokeColor: '#ff0000', strokeOpacity: 0.8, strokeWeight: 2, fillColor: '#ff000', fillOpacity: 0.35}) => {
+	return Promise((resolve, reject) => {
+		if (coordinates && map) {
+
+			// Add coordinates to options
+			options.path = coordinates;
+
+			// Create new Polyline
+			const polygon = new google.maps.Polygon(options);
+
+			// Push to polylines
+			map.polygons.push(polygon);
+
+			// Set the polyline to the map
+			polygon.setMap(map);
+
+			// Return the map
+			resolve(map);
+
+		} else {
+
+			// Reject with an error
+			reject(new Error('coordinates and/or map arguments not given'));
+
+		}
+	});
+};
+
+/**
+ * Remove polygons from map
+ * 
+ * @function
+ * @since 	1.0
+ * @param 	{<google.maps.Map>} map 
+ * @returns {Promise} returns the map object on resolve
+ */
+const removePolygons = (map) => {
+	return new Promise((resolve, reject) => {
+		if (map) {
+
+			// Loop over the polylines
+			map.polygons.forEach((polygon) => {
+				polygon.setMap(null);
+			});
+
+			// Remove markers from array
+			map.polygons = [];
+
+			// Resolve with map
+			resolve(map);
+
+		} else {
+
+			// Reject with an error
+			reject(new Error('map argument not given'));
+
+		}
+	});
+};
+
+
+
 
 
 
