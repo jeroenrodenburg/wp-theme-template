0c48e6eb2fd83f478516d3359578bf7399a357c0
controldigital <info@controldigital.nl>
2018-04-22 12:16:28 +0200

    Added WPML Theme functions file

---
+++ lib/wpml.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ *	Theme:				
+ *	Template:			wpml.php
+ *	Description:		WordPress Multi Language plugin configurations
+ */
+
+define('ICL_DONT_LOAD_NAVIGATION_CSS', true);
+define('ICL_DONT_LOAD_LANGUAGE_SELECTOR_CSS', true);
+define('ICL_DONT_LOAD_LANGUAGES_JS', true);
