872ba65fd873903f5157bac5fdaab1ed49e681a0
controldigital <info@controldigital.nl>
2018-05-01 09:04:22 +0200

    Setup action for rest_api_init

--- lib/rest.php
+++ lib/rest.php
@@ -5,3 +5,16 @@
  *	Description:		REST API Settings
 */
 
+
+/**
+ * Modify REST API responses
+ * 
+ * Register new fields for responses
+ * and modify requests. 
+ * 
+ */
+add_action( 'rest_api_init', 'modify_rest_api' );
+function modify_rest_api() {
+
+}
+
