5d8e0ac38f26e248a3631b15cf068fbae38fef33
controldigital <info@controldigital.nl>
2018-05-01 09:02:44 +0200

    Added custom styling file for admin page

--- lib/admin.php
+++ lib/admin.php
@@ -4,3 +4,13 @@
  *  Template:			admin.php
  *  Description:		Custom admin settings
  */
+
+
+/**
+ * Add custom CSS to the admin page
+ * Enqueues style to admin
+ */
+add_action('admin_enqueue_scripts', 'admin_style');
+function admin_style() {
+	wp_enqueue_style( 'admin_style', get_template_directory_uri() . '/css/admin/admin.css' );
+}
