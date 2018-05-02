33b50032e094d2e6093d9d1532dcc1c673d66a4a
fishfriday <info@fishfriday.nl>
2018-05-02 16:45:34 +0200

    Added meta file

--- functions.php
+++ functions.php
@@ -16,6 +16,7 @@ locate_template( 'lib/filters.php', true, true );         // Filter hooks
 locate_template( 'lib/gf.php', true, true );              // Gravity Forms
 locate_template( 'lib/gutenberg.php', true, true );		  // Gutenberg modifications
 locate_template( 'lib/helpers.php', true, true );         // Helper functions
+locate_template( 'lib/meta.php', true, true );            // Meta functions
 locate_template( 'lib/navigation.php', true, true );      // Navigation registeration and Walkers
 locate_template( 'lib/post-types.php', true, true );      // Custom post types
 locate_template( 'lib/rest.php', true, true );            // Rest settings
