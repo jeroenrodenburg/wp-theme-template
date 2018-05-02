86bdccccf1d7900dadd19446df15647ecf01ba25
fishfriday <info@fishfriday.nl>
2018-05-02 16:48:48 +0200

    Added function that removes default widgets

--- lib/widgets.php
+++ lib/widgets.php
@@ -5,6 +5,26 @@
  *	Description:		Create custom widgets to use in sidebars
  */
 
+/**
+ * Unregister default widgets
+ * 
+ */
+add_action( 'widgets_init', 'unregister_default_widgets' );
+function unregister_default_widgets() {
+	// unregister_widget( 'WP_Widget_Pages' );
+	// unregister_widget( 'WP_Widget_Calendar' );
+	// unregister_widget( 'WP_Widget_Archives' );
+	// unregister_widget( 'WP_Widget_Links' );
+	// unregister_widget( 'WP_Widget_Meta' );
+	// unregister_widget( 'WP_Widget_Search' );
+	// unregister_widget( 'WP_Widget_Text' );
+	// unregister_widget( 'WP_Widget_Categories' );
+	// unregister_widget( 'WP_Widget_Recent_Posts' );
+	// unregister_widget( 'WP_Widget_Recent_Comments' );
+	// unregister_widget( 'WP_Widget_RSS' );
+	// unregister_widget( 'WP_Widget_Tag_Cloud' );
+	// unregister_widget( 'WP_Nav_Menu_Widget' );
+}
 
 /**
  *  Register custom widgets
@@ -12,7 +32,7 @@
  */
 add_action( 'widgets_init', 'register_custom_widgets' );
 function register_custom_widgets() {
-  	register_widget( 'Button_Widget' );
+	register_widget( 'Button_Widget' );
 	register_widget( 'Social_Widget' );
 	register_widget( 'Highlight_Widget' );
 }
