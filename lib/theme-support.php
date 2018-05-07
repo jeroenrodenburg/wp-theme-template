45298d33a607f1e01ce1c8bd3b26be0cbd9c5c0f
controldigital <info@controldigital.nl>
2018-05-07 22:24:46 +0200

    Added all the theme support features

--- lib/theme-support.php
+++ lib/theme-support.php
@@ -1,23 +1,35 @@
 <?php
 /**
- *	Theme:
- *	Template:			theme-support.php
- *	Description:		Set the core functions of the theme
-*/
+ * Theme:
+ * Template:			theme-support.php
+ * Description:			Set the core functions of the theme
+ */
 
 
 /**
- *  Register Theme Features
+ * Register Theme Features
  * 
+ * Setup support for theme features.
+ * Uncomment the features that should not be supported.
+ * 
+ * @since	1.0
+ * @link	https://codex.wordpress.org/Theme_Features
+ * @link	https://developer.wordpress.org/reference/functions/add_theme_support/
  */
 add_action( 'after_setup_theme', 'theme_features' );
 function theme_features()  {
+
+	// Add theme support for title tag
+	add_theme_support( 'title-tag' );
+
+	// Add theme support for post formats
+	add_theme_support( 'post-formats', array( 'aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat' ) );
 
 	// Add theme support for Featured Images
 	add_theme_support( 'post-thumbnails', array( 'post', 'page' ) );
 
 	// Add theme support for HTML5 Semantic Markup
-	add_theme_support( 'html5', array( 'search-form', 'caption' ) );
+	add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
 
 	// Add theme support for Custom Logo
 	add_theme_support( 'custom-logo', array(
@@ -28,7 +40,43 @@ function theme_features()  {
 		'header-text' => array( 'site-title', 'site-description' ),
 	) );
 
-  	// Add theme support for Image Size
+	// Add theme support for Custom Header
+	add_theme_support( 'custom-header', array(
+		'default-image' => '',
+		'random-default' => false,
+		'width' => 0,
+		'height' => 0,
+		'flex-height' => false,
+		'flex-width' => false,
+		'default-text-color' => '',
+		'header-text' => true,
+		'uploads' => true,
+		'wp-head-callback' => '',
+		'admin-head-callback' => '',
+		'admin-preview-callback' => '',
+		'video' => false,
+		'video-active-callback' => 'is_front_page',
+	) );
+
+	// Add theme support for Custom Backgrounds
+	add_theme_support( 'custom-background', array(
+		'default-image' => '',
+		'default-preset' => 'default',
+		'default-position-x' => 'left',
+		'default-position-y' => 'top',
+		'default-size' => 'auto',
+		'default-repeat' => 'repeat',
+		'default-attachment' => 'scroll',
+		'default-color' => '',
+		'wp-head-callback' => '_custom_background_cb',
+		'admin-head-callback' => '',
+		'admin-preview-callback' => '',
+	) );
+
+	// Add theme support for selective refresh of widgets in customizer
+	add_theme_support( 'customize-selective-refresh-widgets' );
+
+	// Add theme support for Image Size
 	// add_image_size( 'title', 0, 0, true );
 
 	// Add theme support for Woocommerce
