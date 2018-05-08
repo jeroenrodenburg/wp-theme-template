31ecc805801f32e2b3f373bdc656c5079bf07fb5
fishfriday <info@fishfriday.nl>
2018-05-08 16:33:07 +0200

    Added insertInlineScript and addNonFuncScript functions

--- js/modules/cookie.js
+++ js/modules/cookie.js
@@ -41,6 +41,7 @@
  * @param 	{Number} expire When cookie expires in days
  * @param 	{String} path Path to store cookie
  * @param   {String} domain The domain to store the cookie
+ * @returns {String} Returns the cookie string
  */
 const setCookie = (name, value, expire, path, domain) => {
     let date = new Date(),
@@ -48,7 +49,7 @@ const setCookie = (name, value, expire, path, domain) => {
     date.setTime(date.getTime() + (expire * 24 * 3600 * 1000));
     expires = date.toUTCString();
     document.cookie = name + '=' + value + '; expires=' + expires + '; path=' + path + '; domain=' + domain;
-    return true;
+    return document.cookie;
 };
 
 /**
@@ -68,7 +69,7 @@ const getCookie = (name) => {
     for (let i = 0; i < cookies.length; i += 1) {
         let currentCookie = cookies[i].trim();
         if (currentCookie.indexOf(nameExpression) == 0) {
-            return currentCookie.substring(nameExpression, currentCookie.length);
+            return currentCookie.substring(nameExpression.length, currentCookie.length);
         }
     }
     return false;
@@ -82,9 +83,96 @@ const getCookie = (name) => {
  * @function
  * @since 	1.0
  * @param 	{String} name Cookie to delete
- * @returns {Boolean}
+ * @returns {String} Returns the cookie
  */
 const deleteCookie = (name) => {
     document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
-    return true;
+    return document.cookie;
+};
+
+/**
+ * insertInlineScript
+ *
+ * Add a script to the part of the document. Inserts the element at the
+ * start of the location.
+ * 
+ * Can be used to load a script or noscript with for Google Tagmanager
+ * or other non functional scripts that can only be loaded when consent
+ * is given
+ * 
+ * @example
+ * let script = '(function(){var foo="bar";}())';
+ * let type = 'script';
+ * let location = document.head;
+ * insertInlineScript(script, type, location);
+ * 
+ * @function
+ * @since   1.0
+ * @param 	{String} script of the script to append
+ * @param	{String} [type] of element (script|noscript)
+ * @param	{HTMLElement} [location] to append
+ * @returns	{(HTMLScriptElement|HTMLElement)} Returns the appended script
+ */
+const insertInlineScript = (script, type = 'script', location = document.head) => {
+    if (script && 'string' === typeof script) {
+        let el = document.createElement(type);
+        el.innerHTML = script;
+        return location.insertBefore(el, location.firstElementChild);
+    }
+    return false;
+};
+
+/**
+ * addNonFuncScript
+ * 
+ * Check the cookies if the cookie exists.
+ * If it exists it checks if the value is the value
+ * of the cookie.
+ * 
+ * When both values are true the function will insert
+ * and array of objects of scripts.
+ * 
+ * @example
+ * window.addEventListener('load', addNonFuncScript, false);
+ * 
+ * @since   1.0
+ * @uses    getCookie
+ * @uses    insertInlineScript
+ * @param   {Event} event
+ */
+const addNonFuncScript = (event) => {
+
+    /** 
+     * Cookie to find.
+     * @type {String} 
+     */
+    const cookieName = 'COOKIENAME';
+
+    /**
+     * Value of cookie to check against .
+     * @type {(String|Boolean)} 
+     */
+    const cookieValue = 'true';
+    
+    /** 
+     * An array with objects that hold the script,
+     * the type of script element and the location
+     * to insert the element to.
+     * 
+     * @type        {Object[]} 
+     * @property    {String} script Script string to insert
+     * @property    {String} type Type of script to insert (script|noscript)
+     * @property    {HTMLElement} location Location to insert script
+     */
+    const scriptsArray = [];
+
+    return ((name, value, scripts) => {
+        let cookie = getCookie(name);
+        if (cookie && cookie === value) {
+            scripts.forEach((script) => {
+                insertInlineScript(script.script, script.type, script.location);
+            });
+        }
+    })(cookieName, cookieValue, scriptsArray);
+
 };
