889cf414a2036da55cfc4c38293cb4161996b688
controldigital <info@controldigital.nl>
2018-07-17 23:15:12 +0200

    Moved inline style and script of splash inside element

--- inc/loader/splash.php
+++ inc/loader/splash.php
@@ -7,47 +7,43 @@
 
 ?>
 
-<style>
-
-    /** 
-    * Modify the styles for
-    * the splash screen
-    */
-
-    #splash {
-        position: fixed;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        opacity: 1;
-        visibility: visible;
-        transition: opacity 350ms ease-in-out, transform 350ms ease-in-out, visibility 350ms ease-in-out;
-        z-index: 99;
-    }
-
-    body.page-ready #splash {
-        opacity: 0;
-        visibility: hidden;
-    }
-
-    html.no-js #splash {
-        display: none;
-    }
-
-</style>
-
-<script>
-
-// Hide splash when page is done loading
-window.addEventListener('load', function (e) { 
-    document.body.classList.add('page-ready'); 
-});
-
-</script>
-
 <!-- Start splash -->
 <div id="splash">
+
+    <style>
+
+        #splash {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            opacity: 1;
+            visibility: visible;
+            transition: opacity 350ms ease-in-out, transform 350ms ease-in-out, visibility 350ms ease-in-out;
+            z-index: 99;
+        }
+
+        body.page-ready #splash {
+            opacity: 0;
+            visibility: hidden;
+        }
+
+        html.no-js #splash {
+            display: none;
+        }
+
+    </style>
+
+    <script>
+
+        // Hide splash when page is done loading
+        window.addEventListener('load', function (e) { 
+            document.body.classList.add('page-ready'); 
+        });
+
+    </script>
+
     <div class="splash-inner"></div>
 </div>
 <!-- End splash -->
