0e676365b3876e1c636e96a1fa63c66f2667f6ad
controldigital <info@controldigital.nl>
2018-07-19 00:23:54 +0200

    Added current navigation class function

--- lib/navigation.php
+++ lib/navigation.php
@@ -23,6 +23,36 @@ function theme_menus() {
 }
 
 /**
+ * add_current_nav_class
+ * 
+ * Make parent menu item from post type
+ * active on single page
+ * 
+ * @since	1.0   
+ */
+add_action( 'nav_menu_css_class', 'add_current_nav_class', 10, 2 );
+function add_current_nav_class( $classes, $item ) {
+    
+    // Getting the current post details
+	global $post;
+	if ( $post === null ) return $classes;
+    
+    // Getting the post type of the current post
+    $current_post_type = get_post_type_object( get_post_type( $post->ID ) );
+    $current_post_type_slug = $current_post_type->rewrite[ 'slug' ];
+        
+    // Getting the URL of the menu item
+    $menu_slug = strtolower( trim( $item->url ) );
+    
+    // If the menu item URL contains the current post types slug add the current-menu-item class
+    if ( strpos( $menu_slug, $current_post_type_slug ) !== false ) $classes[] = 'current-menu-item';
+    
+    // Return the corrected set of classes to be added to the menu item
+    return $classes;
+
+}
+
+/**
  * Custom_Walker_Nav_Menu
  * 
  * Extends the default Walker_Nav_Menu class
