c9846e94badc43206543fee811299de2874fb1ae
controldigital <info@controldigital.nl>
2018-08-09 16:34:29 +0200

    Gutenberg boilerplate

---
+++ js/gutenberg/gutenberg.js
@@ -0,0 +1,47 @@
+/* jshint esversion: 6 */
+
+/**
+ * @author Control <info@controldigital.nl>
+ * @file gutenberg.js
+ * @version 1.0
+ * @license
+ * Copyright (c) 2018 Control.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+const { registerBlockType } = wp.blocks;
+const blockStyle = { 
+    backgroundColor: '#900', 
+    color: '#fff', 
+    padding: '20px' 
+};
+
+registerBlockType( 'gutenberg-boilerplate', {
+    title: 'Hello World (Step 1)',
+    icon: 'universal-access-alt',
+    category: 'layout',
+    edit() {
+        return <p style={ blockStyle }>Hello editor.</p>;
+    },
+    save() {
+        return <p style={ blockStyle }>Hello saved content.</p>;
+    },
+} );
+
--- lib/gutenberg.php
+++ lib/gutenberg.php
@@ -4,3 +4,38 @@
  * Template:			gutenberg.php
  * Description:			Gutenberg modifications
  */
+
+
+/**
+ * gutenberg_boilerplate_block
+ * 
+ * Serverside registration of a new Gutenberg
+ * block for the gutenberg editor
+ * 
+ * @type    action (init)
+ * @since   1.0
+ * 
+ */
+add_action( 'init', 'gutenberg_boilerplate_block' );
+function gutenberg_boilerplate_block() {
+
+    // Register Gutenberg boilerplate JS
+    wp_register_script(
+        'gutenberg-boilerplate',
+        get_template_directory_uri() . '/js/gutenberg/gutenberg.js',
+        array( 'wp-blocks', 'wp-element' )
+    );
+
+    // Register Gutenberg boilerplate CSS
+    wp_register_style(
+        'gutenberg-boilerplate',
+        get_template_directory_uri() . '/css/gutenberg/gutenberg.css',
+        array( 'wp-edit-blocks' )
+    );
+
+    register_block_type( 'gutenberg-boilerplate', array(
+        'editor_script' => 'gutenberg-boilerplate',
+        'editor_style'  => 'gutenberg-boilerplate'
+    ) );
+
+}
