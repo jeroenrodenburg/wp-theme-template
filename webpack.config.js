456bfefa78ae7c2092b673c61aeffeac75cd537d
jeroenrodenburg <jeroen@rodenburg.io>
2018-12-16 17:56:34 +0100

    add postcss config with autoprefixer and scss folder structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,55 @@
-const mode = process.env.NODE_ENV || "development";
-const path = require("path");
+const path = require('path');
 
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const webpack = require("webpack");
-const autoprefixer = require("autoprefixer");
+// include the js minification plugin
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+// include the css extraction and minification plugins
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
-    entry: {
-        "script": "./src/js/script.js",
-    },
-    module: {
-      rules: [
-        {
-          test: /\.scss$/,
-          exclude: /node_modules/,
-          use: ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: ['css-loader', 'sass-loader']
-          })
-        },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
+  entry: ['./src/js/script.js', './src/scss/style.scss'],
+  output: {
+    filename: './dist/script.js',
+    path: path.resolve(__dirname)
+  },
+  module: {
+    rules: [
+      // perform js babelization on all .js files
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
           loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+         }
         }
-      ]
-    },
-    output: {
-    filename: "[name].bundle.js",
-    path: __dirname + "/dist/"
-    },
-    plugins: [
-        new webpack.LoaderOptionsPlugin({
-            options: {
-                postcss: [
-                    autoprefixer()
-                ]
-            }
-        }),
-        new ExtractTextPlugin({
-            filename:  (getPath) => {
-              return getPath('style.css').replace('css/js', 'css');
-            },
-            allChunks: true
-        })
+      },
+      // compile all .scss files to plain old css
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+      }
     ]
-}
+  },
+  plugins: [
+    // extract css into dedicated file
+    new MiniCssExtractPlugin({
+      filename: './dist/style.css'
+    })
+  ],
+  optimization: {
+    minimizer: [
+      // enable the js minification plugin
+      new UglifyJSPlugin({
+        cache: true,
+        parallel: true
+      }),
+    //   enable the css minification plugin
+      new OptimizeCSSAssetsPlugin({
+        preset: ['default', { discardComments: { removeAll: false } }],
+      })
+    ]
+  }
+};
